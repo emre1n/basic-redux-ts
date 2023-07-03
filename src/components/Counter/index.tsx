3b0a7bba5a139b7fbac583482440a8bd85855062
emre1n <emre1n@protonmail.com>
2023-07-03 14:46:23 +0300

    add dispatch functions and buttons

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,10 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './styles.module.css';
 
 import { CounterState } from '../../libs/types';
 
 const Counter = () => {
   const counter = useSelector((state: CounterState) => state.counter);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const handleDecrement = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
 
   const toggleCounterHandler = () => {
     console.log('toggled');
@@ -14,6 +23,10 @@ const Counter = () => {
     <main className={styles.counter}>
       <h1>Redux Counter</h1>
       <div className={styles.value}>{counter}</div>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
