b58522b1ea602c307c85d8319bec2f34ca477997
emre1n <emre1n@protonmail.com>
2023-07-03 14:22:03 +0300

    add redux store and setup provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { Provider } from 'react-redux';
+
 import './index.css';
+import App from './App.tsx';
+import store from './store/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
